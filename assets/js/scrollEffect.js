17c487cb362c91996d19bb64be5fd1df2ccd3dfd
pickausernameg <mb.gui@hotmail.com>
2018-08-29 19:52:41 -0300

    add text to boxes

--- assets/js/scrollEffect.js
+++ assets/js/scrollEffect.js
@@ -1,12 +1,58 @@
 $(document).ready(function(){
+
+/* Must find a way to simplify the scroll effect, 
+thinking if the number of boxes increase */
+
 	$(window).scroll(function(){
-		if ($(this).scrollTop() > 645) {
-			$(".box2").css({"background-attachment" : "fixed"})
+
+		// Set scroll to first box
+		if ($(this).scrollTop() > 670) {
+			$(".box2").css({"background-attachment" : "fixed"});
+			$(".details").css({"position" : "fixed", "top" : "120px", "padding" : "0px 100px"})
 		}
+
 		else {
 			if ($(this).scrollTop() < 800) {
-				$(".box2").css({"background-attachment" : "inherit"})		
+				$(".box2").css({"background-attachment" : "inherit"})
+				$(".details").css({"position" : "unset", "padding" : "120px 100px 0px"})
 			}	
 		}
 	})
-})
+
+	$(window).scroll(function(){
+
+		// Set scroll to second box
+		if ($(this).scrollTop() > 1410) {
+			$(".box3").css({"background-attachment" : "fixed"})
+		}
+
+		else {
+			if ($(this).scrollTop() < 3000) {
+				$(".box3").css({"background-attachment" : "inherit"})		
+			}	
+		}
+	})
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
